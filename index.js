ae8dd3c3bf3b1a38baea37f56ce91d6016be011f
rudghks373 <rudghks373@naver.com>
2019-03-01 19:51:55 +0900

    Updata vsrsion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var app = express();
 
 // DB setting
@@ -18,7 +19,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 // DB schema
 var contactSchema = mongoose.Schema({
   name:{type:String, required:true, unique:true},
@@ -51,6 +52,37 @@ app.post("/contacts", function(req, res){
   });
 });
 
+// Contacts - show // 3
+app.get("/contacts/:id", function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+   if(err) return res.json(err);
+   res.render("contacts/show", {contact:contact});
+  });
+ });
+ 
+ // Contacts - edit // 4 
+ app.get("/contacts/:id/edit", function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+   if(err) return res.json(err);
+   res.render("contacts/edit", {contact:contact});
+  });
+ });
+
+ // Contacts - update // 5 
+ app.put("/contacts/:id", function(req, res){
+  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+   if(err) return res.json(err);
+   res.redirect("/contacts/"+req.params.id);
+  });
+ });
+
+ // Contacts - destroy // 6
+ app.delete("/contacts/:id", function(req, res){
+  Contact.remove({_id:req.params.id}, function(err, contact){
+   if(err) return res.json(err);
+   res.redirect("/contacts");
+  });
+ });
 // Port setting
 var port = 3000;
 app.listen(port, function(){
